5590b0dacb7a08d90068cfd6daf8aa2d6e6ea6ba
garrett-may <garrett.ls.may@gmail.com>
2017-12-18 14:11:05 +0000

    Genetic algorithm appears to be working; need to work on fitness function and probabilities to improve it

--- music_test/ly_to_py.py
+++ music_test/ly_to_py.py
@@ -10,6 +10,7 @@ from music21.pitch import *
 from music21.interval import *
 import copy
 import numpy as np
+import numpy
 from numpy import prod
 from hmmlearn import hmm
 from collections import *
@@ -19,6 +20,7 @@ import random
 from deap import base
 from deap import creator
 from deap import tools
+from deap import algorithms
 
 #environment.set('musicxmlPath', '/mnt/c/Users/garrett-may/Desktop/music_test')
 #environment.set('midiPath', '/mnt/c/Users/garrett-may/Desktop/music_test')
@@ -456,9 +458,9 @@ def cumulative_distribution(chord_types):
         cumulative_dist += [(chord,summed)]
     return cumulative_dist
 
-cumulative_dist = cumulative_distribution(unigrams)
+cumulative_dist = cumulative_distribution(start_p)
 
-def attr_bool(cumulative_dist):
+def attr_float(cumulative_dist):
     r = random.random()
     for (chord,prob) in cumulative_dist:
         if prob >= r:
@@ -482,18 +484,31 @@ def evaluate(individual):
 def mutate(cumulative_dist, individual, indpb):
     for i in range(0, len(individual)):
         if random.random() < indpb:
-            individual[i] = attr_bool(cumulative_dist)
+            individual[i] = attr_float(cumulative_dist)
     return (individual,)
 
 toolbox = base.Toolbox()
-toolbox.register("attr_bool", attr_bool, cumulative_dist)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, len(melody))
+toolbox.register("attr_float", attr_float, cumulative_dist)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, len(melody))
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("evaluate", evaluate)
 toolbox.register("mate", tools.cxTwoPoint)
 toolbox.register("mutate", mutate, cumulative_dist, indpb=0.2)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
+pop = toolbox.population(n=100)
+hof = tools.HallOfFame(1)
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("avg", numpy.mean)
+stats.register("std", numpy.std)
+stats.register("min", numpy.min)
+stats.register("max", numpy.max)
+
+pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=100, stats=stats, halloffame=hof, verbose=True)
+best_ind = tools.selBest(pop, 1)
+print("Gen #{}: best individual is {}".format(100, best_ind))
+
+"""
 pop = toolbox.population(n=10)
 CXPB, MUTPB = 0.5, 0.2
 fitnesses = list(map(toolbox.evaluate, pop))
@@ -523,6 +538,7 @@ while max(fits) < 100 and g < 1:
     best_ind = tools.selBest(pop, len(pop))
     #best_chord = [roman.romanNumeralFromChord(Chord([int(n) for n in c]), Key('C')).romanNumeral for c in best_ind]
     print("Gen #{}: best individual is {}".format(g, best_ind))
+"""
 """
 note_prob = defaultdict(dict)
 for path in corpus.getComposer('bach'):
